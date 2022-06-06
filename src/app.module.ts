15953900ee61a5504469bf02036f63122f19b48d
Herman Wahyudi <hwahyudi@xl.co.id>
2022-06-06 10:09:06 +0700

    add config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
+import { ConfigModule } from './config/config.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/product.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [
+    ConfigModule,
+    // MongoDB Connection
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getMongoConfig(),
+    }),
     ProductsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://herman:hermanw@cls-01.yv0gl.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
-    ),
   ],
   controllers: [AppController],
   providers: [AppService],
